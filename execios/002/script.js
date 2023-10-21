2701d262a45ff822da27e0e93b52cc7590b1b916
WASHINGTON LUIS MARCOS DA SILVA <146831893+Washingtonluismarcos@users.noreply.github.com>
2023-10-21 11:08:53 -0300

    styl

--- execios/002/script.js
+++ execios/002/script.js
@@ -3,17 +3,20 @@ function carregar() {
   var img = window.document.getElementById('imagem')
   var data = new Date()
   //var hora = data.getHours()
-  var hora = 13
+  var hora = 22
   msg.innerHTML = `Now are ${hora} hours.`
   if (hora >= 0 && hora < 12) {
     //Bom dia!
     img.src = 'img/Design sem nome (2).png'
+    document.body.style.background = '#7AB3CC'
   } else if (hora >= 12 && hora < 18){
     //Boa tarde!
     img.src = 'img/Design sem nome (1).png'
     document.body.style.background = '#E78BA8'
   } else {
     //Boa noite
+    
+    document.body.style.background = '#02050E'
     img.src = 'img/Design sem nome (3).png'
   
   }
